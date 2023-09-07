d8cf0acba0b302122d00402ceab6bd2b36a46392
sonell404 <sonell404@gmail.com>
2023-09-07 09:00:35 +0100

    Adjust mobile menu / Adjust mobile Image Viewer

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 
 import Home from "./PAGES/Home";
 import About from "./HEADER/About";
@@ -11,9 +16,13 @@ import "./MainPage.css";
 
 function MainPage() {
   return (
-    <Router>
+    <>
       <HeaderContainer menuOpen={false} />
-      <div className="main-component-container">
+      <div
+        className={`main-component-container ${
+          useLocation().pathname !== "/" ? "active" : ""
+        }`}
+      >
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
@@ -22,7 +31,7 @@ function MainPage() {
         </Routes>
       </div>
       <Footer />
-    </Router>
+    </>
   );
 }
 
