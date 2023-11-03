8d3af83432c6568d8d5200c4ecbb5a7486df14b6
sonell404 <sonell404@gmail.com>
2023-11-03 09:09:16 +0000

    Home Page - Pre-launch

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 
-import Home from "./PAGES/Home";
 import AboutText from "./PAGES/AboutText";
 import HandDrawnWork from "./PAGES/HandDrawnWork";
 import PrintWork from "./PAGES/PrintWork";
@@ -8,11 +7,12 @@ import CodeWork from "./PAGES/CodeWork";
 import HeaderContainer from "./HEADER/HeaderContainer";
 import Footer from "./FOOTER/Footer";
 
-import "./MainPage.css";
 import AboutLogo from "./HEADER/AboutLogo";
 import MainHeader from "./HEADER/MainHeader";
 import MainLogo from "./HEADER/MainLogo";
 import MenuContainer from "./MENU/MenuContainer";
+
+import "./MainPage.css";
 
 function MainPage() {
   const location = useLocation();
@@ -24,7 +24,7 @@ function MainPage() {
           <MainLogo />
           <AboutLogo
             version="header"
-            fillColour={location.pathname === "/about" ? "#ffffff" : "#212121"}
+            fillColour={location.pathname === "/about" || location.pathname === "/" ? "#ffffff" : "#212121"}
           />
         </MainHeader>
       </HeaderContainer>
@@ -35,7 +35,7 @@ function MainPage() {
         }`}
       >
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<AboutText />}></Route>
           <Route path="/about" element={<AboutText />}></Route>
           <Route path="/hand-drawn-work" element={<HandDrawnWork />}></Route>
           <Route path="/print-work" element={<PrintWork />}></Route>
